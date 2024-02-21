11ca31fab49ccb5938ecddea8b94714ab193d476
Nanayawagyemang <159439709+Nanayawagyemang@users.noreply.github.com>
2024-02-21 00:04:06 +0000

    Modify:Modified database.py

--- database.py
+++ database.py
@@ -2,12 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todo.db"
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:Qwerty123@localhost:3306/todoapp'
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
