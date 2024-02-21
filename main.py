de90a375bd0afe9e45700c854167291f3c3a8578
Nanayawagyemang <159439709+Nanayawagyemang@users.noreply.github.com>
2024-02-21 00:04:29 +0000

    Modify:Modified main.py

--- main.py
+++ main.py
@@ -2,26 +2,18 @@ from fastapi import Depends
 from fastapi import FastAPI, Form
 from fastapi import Request, Response
 from fastapi.responses import HTMLResponse
-
+from router import router
 from fastapi.templating import Jinja2Templates
-from sqlalchemy.orm import Session
 
-from database import Base
-from database import SessionLocal
-from database import engine
+app = FastAPI(
+    title="Todo-App",
+    description=f"Api documentation for NY Todo API",
+    version="0.1.0",
+)
 
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(router)
 
 @app.get("/")
 async def index():
